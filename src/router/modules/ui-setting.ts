f6e5063f6fa93a27685b6af81a9c20415b1bffd1
吴梦成 <wumengcheng@wumengchengdeMacBook-Pro.local>
2025-06-13 11:38:10 +0800

    init

--- src/router/modules/ui-setting.ts
+++ src/router/modules/ui-setting.ts
@@ -30,7 +30,7 @@ export default {
   children: [
     {
       path: '',
-      name: 'ui-setting',
+      name: 'ui-setting-home',
       component: components['ui-setting'],
       meta: {
         title: '设置',
